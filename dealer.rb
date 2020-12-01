17ca8b10e054f32f81d6834bd166532ff33ae374
Andex <anyanov1998@gmail.com>
2020-12-01 22:47:25 +0300

    Add choice_strategy for dealer

--- dealer.rb
+++ dealer.rb
@@ -2,4 +2,17 @@ class Dealer < Player
   def initialize
     super('dealer')
   end
+
+  def choice_strategy
+    amount = points.inject(0, :+)
+    ace = ace_position
+    if amount > 21 && ace
+      points[ace] = 1
+      choice_strategy
+    elsif amount < 17 && cards.size < 3
+      actions[1]
+    else
+      actions[0]
+    end
+  end
 end
