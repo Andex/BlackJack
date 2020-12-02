d6cd1b3bfdf54a4b00131f46a204874852706cab
Andex <anyanov1998@gmail.com>
2020-12-02 21:47:51 +0300

    Рефактор choice_strategy

--- dealer.rb
+++ dealer.rb
@@ -3,16 +3,30 @@ class Dealer < Player
     super('dealer')
   end
 
+  # def choice_strategy
+  #   amount = points.inject(0, :+)
+  #   ace = ace_position
+  #   if amount > 21 && ace
+  #     points[ace] = 1
+  #     choice_strategy
+  #   elsif amount < 17 && cards.size < 3
+  #     1
+  #   else
+  #     0
+  #   end
+  # end
+
+  # def choice_action
+  #   if amount < 17 && cards.size < 3
+  #     take_card(deck)
+  #   else
+  #     0
+  #   end
+  # end
+
   def choice_strategy
+    choice_strategy_points
     amount = points.inject(0, :+)
-    ace = ace_position
-    if amount > 21 && ace
-      points[ace] = 1
-      choice_strategy
-    elsif amount < 17 && cards.size < 3
-      actions[1]
-    else
-      actions[0]
-    end
+    return 'take_card' if amount < 17 && cards.size < 3
   end
 end
