31b665cdeb9a323a075fbe51ed7aebfb4bb52064
Andex <anyanov1998@gmail.com>
2020-12-02 21:45:51 +0300

    Add show_winner

--- interface.rb
+++ interface.rb
@@ -1,4 +1,5 @@
 class Interface
+  attr_reader :actions
   attr_accessor :player, :dealer, :current_game
 
   def initialize
@@ -16,22 +17,23 @@ class Interface
       current_game.end_game
       p 'Карты диллера:'
       dealer.view_hand
-      print 'Победитель ', current_game.winner
-      if current_game.winner == player
-        p "#{player.name}, Вы выиграли #{current_game.bank} долларов!"
-      elsif current_game.winner == dealer
-        p 'К сожалению, Вы проиграли'
-      else
-        p 'Ничья! Деньги из банка делятся пополам'
-      end
+      show_winner
       return p "Вы закончили игру с активом в #{player.bank} долларов" unless new_game?
 
       self.current_game = Game.new(player, dealer)
       first_choice
     end
-    p 'Ход диллера...'
-    dealer.choice_strategy
+    dealer_move
     act(choise_action)
+  end
+
+  def dealer_move
+    p 'Дилер делает ход...'
+    action = dealer.choice_strategy
+    p "Dealer action = #{action}"
+    dealer.take_card(current_game.deck) if action == 'take_card'
+    p 'Карты дилера:'
+    dealer.cards.count.times { print '*' }
   end
 
   def first_choice
@@ -55,17 +57,16 @@ class Interface
   end
 
   def choise_action
-    p 'Сейчас Ваш ход', 'У Вас на руках следующие карты:'
+    p '\\nnСейчас Ваш ход', 'У Вас на руках следующие карты:'
     player.view_hand
+    p "В сумме #{player.amount} очков"
     player.actions.each_with_index { |action, ind| p "#{ind + 1} - #{action}" }
     gets.chomp.to_i - 1
   end
 
   def take_card
     player.take_card(current_game.deck)
-    p 'Теперь у Вас руках 3 карты:'
-    player.view_hand
-    p "В сумме #{player.amount} очков"
+    p 'Теперь у Вас на руках 3 карты'
   rescue RuntimeError
     p 'У Вас уже 3 карты, больше брать нельзя'
     act(choise_action)
@@ -74,5 +75,15 @@ class Interface
   def new_game?
     p 'Хотите продолжить? Если да, введите 1, иначе 0'
     !gets.chomp.to_i.zero?
+  end
+
+  def show_winner
+    if current_game.winner == player
+      p "#{player.name}, Вы выиграли #{current_game.bank} долларов!"
+    elsif current_game.winner == dealer
+      p 'К сожалению, Вы проиграли'
+    else
+      p 'Ничья! Деньги из банка делятся пополам'
+    end
   end
 end
